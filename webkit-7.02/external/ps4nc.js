81985f72796210144c7c2e0b8c0858aa4cd9a511
RVC <ronnevic@gmail.com>
2021-03-14 01:43:46 -0600

    Fixes!!

--- webkit-7.02/external/ps4nc.js
+++ webkit-7.02/external/ps4nc.js
@@ -99,8 +99,7 @@ function setupRW() {
 		die("[!] Failed to setup arbitrary R/W primitive");
 
 	debug_log("[+] Succesfully got arbitrary R/W!");
-	debug_log("[+] PS4 has been Jailbroken!");
-	alert("waiting for payload/NetCat Port 9020");
+
 
 	/* Restore the overidden vtable pointer */
 	write64(guess_htmltextarea_addr, saved_vtable);
@@ -117,6 +116,8 @@ function setupRW() {
 	if(!read64(g_jsview_butterfly.sub(16)).equals(new Int64("0xffff000000001337")))
 		die("[!] Failed to setup addrof/fakeobj primitives");
 	debug_log("[+] Succesfully got addrof/fakeobj");
+	debug_log("[+] PS4 has been Jailbroken!");
+	alert("waiting for payload/NetCat Port 9020");
 
 	/* Getting code execution */
 	/* ... */
